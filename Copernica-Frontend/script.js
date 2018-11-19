fec60f6002f309613ae5c2b592ad7178c23e67f9
Niels Tromp <nielsfcg1@gmail.com>
2018-11-19 13:31:12 +0100

    step1
    Beginning CSS HTML
    Everything aligned.
    
    next step: overlay

---
+++ Copernica-Frontend/script.js
@@ -0,0 +1,45 @@
+// placeholder avatar in the middle of the screen
+
+// click event
+
+// outline image(1px hover)
+
+// popup menu
+// groups of 4 aligned left
+
+// selection highlight
+
+// switch image
+
+// loading animation + wait timer(http request, blue 3 px, 1 second or random)
+
+// close menu
+
+// highligth avatar in menu (blue border 3px)
+// border other avatar on hover (grey 3px opacity 20%)
+
+// loopable
+
+// remember selection
+
+// animating popup menu
+
+// mouse cursor
+
+var popoverController = (function(){
+
+})();
+
+
+
+
+var dataController = (function(){
+
+})();
+
+
+
+
+var appController = (function(){
+
+})();
