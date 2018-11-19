fd857dac342cc6ceb3244736cbd9617a24f5fae0
Niels Tromp <nielsfcg1@gmail.com>
2018-11-19 19:39:30 +0100

    Working on loading animation

--- Copernica-Frontend/script.js
+++ Copernica-Frontend/script.js
@@ -1,10 +1,6 @@
 
-// click event
 
-
-
-
-
+// loading animation
 
 // switch image
 
@@ -14,9 +10,9 @@
 
 // remember selection
 
-// animating popup menu
-
 // mouse cursor
+
+// click outside element
 
 
 
@@ -26,22 +22,25 @@ var appController = (function(){
     var elements = {
         avatar: '.selected_avatar',
         popover: '.popover',
+        selectAvatar: '.avatar',
         shown: false,
     }
 
     var setupEventListeners = function()
-    {    console.log('setup');
+    {    
         if(document.querySelector(elements.popover)){
             document.querySelector(elements.avatar).addEventListener('click', expandMenu);
         }
+
+       document.querySelector(elements.selectAvatar).addEventListener('click', loadAnimation);
     }
 
 
     expandMenu = function()
-    {   console.log('expandMenu');
-    if(elements.shown === false){
-        document.querySelector(elements.popover).classList.add("animate");
-        elements.shown = true;
+    {   
+        if(elements.shown === false){
+            document.querySelector(elements.popover).classList.add("animate");
+            elements.shown = true;
         }else{
                 hideMenu();
                 elements.shown = false;
@@ -52,11 +51,37 @@ var appController = (function(){
         document.querySelector(elements.popover).classList.remove ('animate');
     }
 
+    loadAnimation = function(el){
+        console.log(el);
+        switch(el){
+            case '1':
+            document.getElementById('1').classList.add("animate");
+            console.log('added');
+            break;
+            case '2':
+            document.getElementById('2').classList.add("animate");
+            break;
+            case '3':
+            document.getElementById('3').classList.add("animate");
+            break;
+            case '4':
+            document.getElementById('4').classList.add("animate");
+            break;
+            case '5':
+            document.getElementById('5').classList.add("animate");
+            break;
+            case '6':
+            document.getElementById('6').classList.add("animate");
+            break;
+
+        }
+    }
+
 return {
     init: function()
     {
         console.log('Application has started');
-     setupEventListeners();
+        setupEventListeners();
     }
 }
 
