febb09e1e6af7977c23af674d402207528cf66f1
Niels Tromp <nielsfcg1@gmail.com>
2018-11-20 10:36:51 +0100

    selection working

--- Copernica-Frontend/script.js
+++ Copernica-Frontend/script.js
@@ -1,11 +1,12 @@
-
+//****DONE*** */
 
 // loading animation
 // style border seprately
+// loading animation + wait timer(http request, blue 3 px, 1 second or random
+
+//**** TO DO */
 
 // switch image
-
-// loading animation + wait timer(http request, blue 3 px, 1 second or random)
 
 // loopable
 
@@ -28,9 +29,9 @@ var appController = (function(){
         selectAvatar: '.avatar',
         selectBorder: '.border',
         animateBorder: 'border.animateBorder',
-        shown: false,
-    
+        shown: false,    
     }
+    var newAvatar = 0;
 
     var setupEventListeners = function()
     {    
@@ -39,14 +40,11 @@ var appController = (function(){
         }
         
         document.querySelector(elements.selectBorder).addEventListener('click', loadAnimation);
-        var x = document.querySelectorAll(elements.selectBorder);
-        //.addEventListener("webkitAnimationEnd", hideMenu);
-       console.log(x);
-       for(var i = 0; i< x.length; i++){
-           x[i].addEventListener('webkitAnimationEnd', hideMenu);
-       }
-       
         
+        var whichAvatar = document.querySelectorAll(elements.selectBorder);
+            for(var i = 0; i< whichAvatar.length; i++){
+                whichAvatar[i].addEventListener('webkitAnimationEnd', hideMenu);
+            }        
     }
 
 
@@ -54,7 +52,7 @@ var appController = (function(){
     {   
         if(elements.shown === false){
             document.querySelector(elements.popover).classList.add("animate");
-            // andere class
+            
             elements.shown = true;
         }else{
                 hideMenu();
@@ -63,34 +61,52 @@ var appController = (function(){
     }
 
     hideMenu = function() {
+        changeAvatar();
         document.querySelector(elements.popover).classList.remove ('animate');
         document.querySelector(elements.selectBorder).classList.remove('animateBorder');
         elements.shown = false;
-        //is een andere class geworden
+        
+        
     }
+
     // el get passed from CSS div
     loadAnimation = function(el){
+        console.log(el);
         switch(el){
             case '1':
             document.getElementById('border_1').classList.add("animateBorder");
+            newAvatar = '1';
             break;
             case '2':
             document.getElementById('border_2').classList.add("animateBorder");
+            newAvatar = '2';
             break;
             case '3':
             document.getElementById('border_3').classList.add("animateBorder");
+            newAvatar = '3';
             break;
             case '4':
             document.getElementById('border_4').classList.add("animateBorder");
+            newAvatar = '4';
             break;
             case '5':
             document.getElementById('border_5').classList.add("animateBorder");
+            newAvatar = '5';
             break;
             case '6':
             document.getElementById('border_6').classList.add("animateBorder");
+            newAvatar = '6';
             break;
 
         }
+    }
+
+    changeAvatar = function()
+    {
+        document.querySelector(elements.avatar).src='images/img' + newAvatar + '.png';
+        elements.avatar
+        //whichAvatar[i]
+
     }
 
     
